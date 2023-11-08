ca3e715e9403c0e11f9080da45ddeff0655dd9c6
Buratud Thumboon <buratud@gmail.com>
2023-11-08 09:04:20 +0700

    Rename folder

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -10,6 +10,9 @@
 #include "AC.hpp"
 #include "Type.cpp"
 
+// Define constants and variables
+
+// Certificate for secure WiFi connection
 const char *ca PROGMEM = R"(-----BEGIN CERTIFICATE-----
 MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
 TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
@@ -41,71 +44,75 @@ oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
 mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
 emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
 -----END CERTIFICATE-----)";
-
+// Pin configurations for IR communication
 const uint16_t IR_RECEIVE_PIN PROGMEM = 34; // The ESP GPIO pin to use that controls the IR LED.
 const uint16_t IR_SEND_PIN PROGMEM = 32;    // The ESP GPIO pin to use that controls the IR LED.
 const uint16_t CAPTURE_BUFFER_SIZE PROGMEM = 1024;
 const uint8_t CAPTURE_TIMEOUT PROGMEM = 50;              // This is the gap timeout
 const uint8_t TOLERANCE_PERCENTAGE PROGMEM = kTolerance; // kTolerance is normally 25%
 
+// WiFi and network configurations
 const char *SSID PROGMEM = "SSID";
 const char *PASSWORD PROGMEM = "PASSWORD";
 const char *WEBSOCKET_SERVER PROGMEM = "linux-vm-southeastasia-1.southeastasia.cloudapp.azure.com";
 const int WEBSOCKET_PORT PROGMEM = 8080;
-const int MQTT_PORT PROGMEM = 8883;
 
+// IR communication objects
 decode_type_t protocol = decode_type_t::UNKNOWN;
 decode_results results;
+AC ac(IR_SEND_PIN);     // Object to control the AC unit
+IRrecv irrecv(IR_RECEIVE_PIN, CAPTURE_BUFFER_SIZE, CAPTURE_TIMEOUT, true);  // Object to receive IR signals
 
-AC ac(IR_SEND_PIN);
-IRrecv irrecv(IR_RECEIVE_PIN, CAPTURE_BUFFER_SIZE, CAPTURE_TIMEOUT, true);
+WiFiClientSecure wificlient;    // Secure WiFi client object
+WebSocketsClient webSocket;     // WebSocket client object
 
-WiFiClientSecure wificlient;
-WebSocketsClient webSocket;
-
+// Function prototypes
 void handlePing();
 void handleCommand(DynamicJsonDocument);
 void handleIncomingText(uint8_t*);
 
+// Setup function, runs once at the beginning of the program
 void setup()
 {
-  Serial.begin(921600);
+  Serial.begin(921600);  // Initialize the serial communication
   delay(1000);
-  wificlient.setCACert(ca);
-  connectWiFi();
-  irrecv.setTolerance(TOLERANCE_PERCENTAGE);
-  irrecv.enableIRIn();
-  webSocket.beginSSL(WEBSOCKET_SERVER, WEBSOCKET_PORT, "/model");
-  webSocket.onEvent(webSocketEvent);
+  wificlient.setCACert(ca);  // Set the CA certificate for secure WiFi connection
+  connectWiFi();  // Connect to the WiFi network
+  irrecv.setTolerance(TOLERANCE_PERCENTAGE);  // Set the tolerance for IR signals
+  irrecv.enableIRIn();  // Enable IR receiver
+  webSocket.beginSSL(WEBSOCKET_SERVER, WEBSOCKET_PORT, "/model");  // Set up WebSocket client
+  webSocket.onEvent(webSocketEvent);  // Register event handler for WebSocket events
 }
 
+// Main loop function, runs repeatedly after setup
 void loop()
 {
-  if (!isWiFiConnected())
+  if (!isWiFiConnected())  // If WiFi connection is lost, attempt to reconnect
   {
     Serial.println("WiFi connection lost. Attempting to reconnect.");
     WiFi.reconnect();
   }
   else
   {
-    if (irrecv.decode(&results))
+    if (irrecv.decode(&results))  // If an IR signal is received
     {
-      Serial.println(typeToString(results.decode_type));
+      Serial.println(typeToString(results.decode_type));  // Print the type of the received IR signal
       if (results.decode_type != decode_type_t::UNKNOWN)
       {
         String output;
         protocol = results.decode_type;
         StaticJsonDocument<256> jsonDocument;
         jsonDocument["type"] = TYPE_PROTOCAL;
-        jsonDocument["value"]["model"] = (int)protocol;
+        jsonDocument["value"]["protocol"] = (int)protocol;
         serializeJson(jsonDocument, output);
-        webSocket.sendTXT(output);
+        webSocket.sendTXT(output);  // Send the received IR signal type to the WebSocket server
       }
     }
-    webSocket.loop();
+    webSocket.loop();  // Keep the WebSocket connection alive
   }
 }
 
+// Event handler for WebSocket events
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
 {
   switch (type)
@@ -116,12 +123,12 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
   case WStype_CONNECTED:
   {
     Serial.printf("[WSc] Connected to url: %s\n", payload);
-    webSocket.sendTXT("{ \"type\": \"status\", \"value\": { \"status\": 1 } }");
+    webSocket.sendTXT("{ \"type\": \"status\", \"value\": { \"status\": true } }");  // Send a status message to the WebSocket server
   }
   break;
   case WStype_TEXT:
     Serial.printf("[WSc] get text: %s\n", payload);
-    handleIncomingText(payload);
+    handleIncomingText(payload);  // Handle incoming text message from the WebSocket server
     break;
   case WStype_BIN:
     Serial.printf("[WSc] get binary length: %u\n", length);
@@ -135,12 +142,13 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
   }
 }
 
+// Connect to the WiFi network
 void connectWiFi()
 {
   Serial.print("Attempting to connect to SSID: ");
   Serial.println(SSID);
   WiFi.begin(SSID, PASSWORD);
-  // attempt to connect to Wifi network:
+  // Attempt to connect to the Wifi network:
   while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print(".");
@@ -151,39 +159,43 @@ void connectWiFi()
   Serial.println(SSID);
 }
 
+// Check if WiFi is connected
 bool isWiFiConnected()
 {
   return WiFi.status() == WL_CONNECTED;
 }
 
+// Send a pong message in response to a ping message
 void handlePing()
 {
   webSocket.sendTXT("{\"type\":\"pong\"}");
 }
 
+// Handle received command from the WebSocket server
 void handleCommand(DynamicJsonDocument jsonDocument)
 {
-  int protocol = jsonDocument["protocol"];
-  bool power = jsonDocument["power"];
+  int protocol = jsonDocument["value"]["protocol"];
+  bool power = jsonDocument["value"]["power"];
   int mode = (int)stdAc::opmode_t::kCool;
-  int degrees = jsonDocument["degrees"];
-  int fanspeed = jsonDocument["fanspeed"];
-  int swingv = jsonDocument["swingv"];
-  int swingh = jsonDocument["swingh"];
-  bool econo = jsonDocument["econo"];
-  bool turbo = jsonDocument["turbo"];
-  ac.sendAc(power, protocol, mode, degrees, fanspeed, swingv, swingh, econo, turbo);
+  int degrees = jsonDocument["value"]["degrees"];
+  int fanspeed = jsonDocument["value"]["fanspeed"];
+  int swingv = jsonDocument["value"]["swingv"];
+  int swingh = jsonDocument["value"]["swingh"];
+  bool econo = jsonDocument["value"]["econo"];
+  bool turbo = jsonDocument["value"]["turbo"];
+  ac.sendAc(power, protocol, mode, degrees, fanspeed, swingv, swingh, econo, turbo);  // Send the AC command to the AC unit
 }
 
+// Handle incoming text message from the WebSocket server
 void handleIncomingText(uint8_t* payload) {
    DynamicJsonDocument jsonDocument(1024);
     deserializeJson(jsonDocument, payload);
-    if (jsonDocument["type"] = TYPE_PING)
+    if (jsonDocument["type"] == TYPE_PING)
     {
-      handlePing();
+      handlePing();  // Handle ping message
     }
-    else if (jsonDocument["type"] = TYPE_COMMAND)
+    else if (jsonDocument["type"] == TYPE_COMMAND)
     {
-      handleCommand(jsonDocument["type"]);
+      handleCommand(jsonDocument);  // Handle command message
     }
 }
